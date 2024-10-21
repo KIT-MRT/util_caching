80bc6cea4adbc5947910f48e472bf8102d86cae8
Nick Le Large <nick.lelarge@kit.edu>
2024-10-21 10:31:09 +0200

    Use parameter packs for policy binding. Split binding functions into separate namespaces.

--- include/util_caching/python_bindings.hpp
+++ include/util_caching/python_bindings.hpp
@@ -10,22 +10,40 @@ namespace util_caching::python_api {
 
 namespace py = pybind11;
 
+namespace number_based {
+namespace internal {
+template <typename CacheT, typename NumberT, typename... ComparisonPolicyTs>
+void bindPolicies(py::class_<CacheT, std::shared_ptr<CacheT>>& cacheClass) {
+    (cacheClass.def(
+         "cached",
+         [](CacheT& self, const NumberT& key, const ComparisonPolicyTs& policy) {
+             return self.template cached<ComparisonPolicyTs>(key, policy);
+         },
+         py::arg("key"),
+         py::arg("policy")),
+     ...);
+}
+} // namespace internal
+
+template <typename NumberT>
+void bindApproximatePolicy(py::module& module, const std::string& name = "ApproximateNumber") {
+    using ApproximateNumberT = policies::ApproximateNumber<NumberT>;
+    py::class_<ApproximateNumberT, std::shared_ptr<ApproximateNumberT>>(module, name.c_str())
+        .def(py::init<NumberT>(), py::arg("threshold"))
+        .def("__call__", &ApproximateNumberT::operator(), "Compare two numbers");
+}
+
 /*!
  * \brief Bindings for a Cache that is based on number comparisons
  *
  * This function binds the Cache class for a specific number-based key type (NumberT) and value type (ValueT).
  * Call this function once inside PYBIND11_MODULE macro to create a Python module with the bindings.
  */
-template <typename NumberT, typename ValueT, typename ThresholdNumberT = NumberT>
-void bindNumberBasedCache(py::module& module) {
+template <typename NumberT, typename ValueT, typename... ComparisonPolicies>
+void bindCache(py::module& module) {
     using CacheT = Cache<NumberT, ValueT>;
-    using ApproximateNumberT = policies::ApproximateNumber<ThresholdNumberT>;
-
-    py::class_<ApproximateNumberT, std::shared_ptr<ApproximateNumberT>>(module, "ApproximateNumber")
-        .def(py::init<ThresholdNumberT>(), py::arg("threshold"))
-        .def("__call__", &ApproximateNumberT::operator(), "Compare two values");
-
-    py::class_<CacheT, std::shared_ptr<CacheT>>(module, "Cache")
+    py::class_<CacheT, std::shared_ptr<CacheT>> cache(module, "Cache");
+    cache
         .def(py::init<>())
         // We cannot pass template parameters to python functions, therefore we need to explicitly bind all
         // instantiations to different python functions.
@@ -34,17 +52,36 @@ void bindNumberBasedCache(py::module& module) {
             "cached",
             [](CacheT& self, const NumberT& key) { return self.template cached<std::equal_to<NumberT>>(key); },
             py::arg("key"))
-        // Approximate match: using ApproximateNumber policy
-        .def(
-            "cached",
-            [](CacheT& self, const NumberT& key, const ApproximateNumberT& policy) {
-                return self.template cached<ApproximateNumberT>(key, policy);
-            },
-            py::arg("key"),
-            py::arg("policy"),
-            "Check if the value is approximately cached based on the given policy")
         .def("cache", &CacheT::cache, py::arg("key"), py::arg("value"))
         .def("reset", &CacheT::reset);
+
+    internal::bindPolicies<CacheT, NumberT, ComparisonPolicies...>(cache);
+}
+
+} // namespace number_based
+
+namespace time_based {
+
+namespace internal {
+template <typename CacheT, typename TimeT, typename... ComparisonPolicyTs>
+void bindPolicies(py::class_<CacheT, std::shared_ptr<CacheT>>& cache) {
+    (cache.def(
+         "cached",
+         [](CacheT& self, const TimeT& key, const ComparisonPolicyTs& policy) {
+             return self.template cached<ComparisonPolicyTs>(key, policy);
+         },
+         py::arg("key"),
+         py::arg("policy")),
+     ...);
+}
+} // namespace internal
+
+template <typename TimeT, typename ThresholdTimeUnitT>
+void bindApproximatePolicy(py::module& module, const std::string& name = "ApproximateTime") {
+    using ApproximateTimeT = policies::ApproximateTime<TimeT, ThresholdTimeUnitT>;
+    py::class_<ApproximateTimeT, std::shared_ptr<ApproximateTimeT>>(module, name.c_str())
+        .def(py::init<double>(), py::arg("threshold"))
+        .def("__call__", &ApproximateTimeT::operator(), "Compare two time points");
 }
 
 /*!
@@ -53,31 +90,21 @@ void bindNumberBasedCache(py::module& module) {
  * This function binds the Cache class for a specific time-based key type (TimeT) and value type (ValueT).
  * Call this function once inside PYBIND11_MODULE macro to create a Python module with the bindings.
  */
-template <typename TimeT, typename ValueT, typename ThresholdTimeUnitT = std::chrono::milliseconds>
-void bindTimeBasedCache(py::module& module) {
+template <typename TimeT, typename ValueT, typename... ComparisonPolicyTs>
+void bindCache(py::module& module) {
     using CacheT = Cache<TimeT, ValueT>;
-    using ApproximateTimeT = policies::ApproximateTime<TimeT, ThresholdTimeUnitT>;
 
-    py::class_<ApproximateTimeT>(module, "ApproximateTime")
-        .def(py::init<double>(), py::arg("threshold"))
-        .def("__call__", &ApproximateTimeT::operator(), "Compare two time points");
-
-    py::class_<CacheT, std::shared_ptr<CacheT>>(module, "Cache")
-        .def(py::init<>())
+    py::class_<CacheT, std::shared_ptr<CacheT>> cache(module, "Cache");
+    cache.def(py::init<>())
         .def(
             "cached",
             [](CacheT& self, const TimeT& key) { return self.template cached<std::equal_to<TimeT>>(key); },
             py::arg("key"))
-        .def(
-            "cached",
-            [](CacheT& self, const TimeT& key, const ApproximateTimeT& policy) {
-                return self.template cached<ApproximateTimeT>(key, policy);
-            },
-            py::arg("key"),
-            py::arg("policy"),
-            "Check if the value is approximately cached based on the given policy")
         .def("cache", &CacheT::cache, py::arg("key"), py::arg("value"))
         .def("reset", &CacheT::reset);
+
+    internal::bindPolicies<CacheT, TimeT, ComparisonPolicyTs...>(cache);
 }
 
+} // namespace time_based
 } // namespace util_caching::python_api
--- test/python_bindings.cpp
+++ test/python_bindings.cpp
@@ -2,6 +2,7 @@
 
 #include "cache.hpp"
 #include "python_bindings.hpp"
+#include "types.hpp"
 
 namespace py = pybind11;
 
@@ -9,8 +10,11 @@ using namespace util_caching;
 
 PYBIND11_MODULE(util_caching_py, mainModule) {
     py::module numberBased = mainModule.def_submodule("number_based");
-    python_api::bindNumberBasedCache<double, double>(numberBased);
+    python_api::number_based::bindApproximatePolicy<double>(numberBased);
+    python_api::number_based::bindCache<double, double, policies::ApproximateNumber<double>>(numberBased);
 
     py::module timeBased = mainModule.def_submodule("time_based");
-    python_api::bindTimeBasedCache<Time, double, std::chrono::milliseconds>(timeBased);
+    python_api::time_based::bindApproximatePolicy<Time, std::chrono::milliseconds>(timeBased);
+    python_api::time_based::bindCache<Time, double, policies::ApproximateTime<Time, std::chrono::milliseconds>>(
+        timeBased);
 }
